0f5f0da9ff44fcaceed5a1c7e8f79dcc0f38fc45
Ashkhatoyan <Albert.Ashkhatoyan@veritas.com>
2021-09-06 16:46:35 +0400

    Update2

--- print.py
+++ print.py
@@ -2,7 +2,9 @@ name = "Albert"
 age = 22
 bro = "Hrut"
 broage = 16
-years = (age - broage)
 print ("My name is "+name+". I am " + str(age)+" years old!")
 print ("Bros name is "+bro+" He is "+ str(broage)+ "years old")
-print ("I am older "+str(years)+" years!")
+if age > broage:
+ print (name+ " is older")
+else:
+ print( bro+" is older")
