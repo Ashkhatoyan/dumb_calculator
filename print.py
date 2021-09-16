dabc99adb1a7fdea83029c90436db3bdbfa307e7
Ashkhatoyan <Albert.Ashkhatoyan@veritas.com>
2021-09-16 14:39:54 +0400

    last commit

--- print.py
+++ print.py
@@ -1,10 +1,11 @@
-#Basic Calculator
-what = input ("what we need to do?(+,-):")
-a = float(input("First number:"))
-b = float(input ("second number:"))
-if what == "+":
-     c = a + b
-     print (c)
-elif what == "-":
-    c = a- b 
-    print(c)
+age = 22
+bro = "Hrut"
+broage = 16
+years = (age - broage)
+print ("My name is "+name+". I am " + str(age)+" years old!")
+print ("Bros name is "+bro+" He is "+ str(broage)+ "years old")
+print ("I am older "+str(years)+" years!") 
+if age > broage:
+ print (name+ " is older")
+else:
+ print( bro+" is older") 
